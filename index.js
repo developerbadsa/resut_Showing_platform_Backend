9af063bd9ca896e22e47eff1f76176deaf0ca4d9
Rahim Badsa <developerbadsa@gmail.com>
2024-04-25 09:06:10 +0600

    origin added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(
    cors({
-      origin: ['http://localhost:5173', 'https://result-showing-platform-frontend.vercel.app']
+      origin: ['http://localhost:5173', 'https://result-showing-platform-frontend.vercel.app', 'https://willowy-dasik-f78ba2.netlify.app']
    })
 );
 
